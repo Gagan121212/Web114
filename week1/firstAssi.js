bd7f0696106db043916d45c4ed809a9bf2116337
Gagan121212 <glakandr@stumail.jccc.edu>
2025-10-12 13:54:29 -0500

    Add files via upload

---
+++ week1/firstAssi.js
@@ -0,0 +1,9 @@
+let fname = "Gagan Lakandri";
+let favoriteColor = "Yellow";
+let hobby = "watching Movies";
+let food = "MOMO";
+let animal = "Dog";
+
+console.log("My name is " + fname + ". My favorite color is " +
+favoriteColor + ". My hobby is " + hobby + " and I love to eat " +
+food + ". " + animal + " is my favorite animal.");
